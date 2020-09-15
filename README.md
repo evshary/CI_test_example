9fc1d7d4887ff8de5b3a9e4250e805e42d73e223
ChenYing Kuo <evshary@gmail.com>
2020-09-15 11:31:12 +0800

    Add initial code for test.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # test_pool
+This repo is useless, just for me test some function like travis.yml and codecov.
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int func()
+{
+    printf("func\n");
+}
+
+int main()
+{
+    printf("Hello World\n");
+    func();
+    return 0;
+}
